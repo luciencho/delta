0131232ba440d32d28ef32f0de59c274e92684c7
zhaoyao <zhaoyao@kingsoft.com>
2018-07-24 14:29:37 +0800

    add num_trees

--- src/dual_encoder/model.py
+++ src/dual_encoder/model.py
@@ -171,6 +171,7 @@ class DualEncoderModel(RetrievalModel):
 def solo_lstm():  # 3.114 30.39%
     hparams = tf.contrib.training.HParams(
         num_keywords=10000,
+        num_trees=10,
         word_size=50000,
         char_size=5000,
         emb_dim=256,
